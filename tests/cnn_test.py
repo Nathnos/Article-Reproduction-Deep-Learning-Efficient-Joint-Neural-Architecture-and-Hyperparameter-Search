01e82a29c25149a2ab02cdaeef60f13c7e63859a
VILLARD Lorenzo <villard.lorenzo.comptes@protonmail.com>
2020-09-19 15:14:50 +0200

    Fixed the model loading, and more comments

--- tests/cnn_test.py
+++ tests/cnn_test.py
@@ -1,17 +1,37 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model, save_model
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 
-INPUT_SIZE = (64, 64)
-NEW_MODEL = True
+INPUT_SIZE = (64, 64) #(128, 128) #For pictures
+NEW_MODEL = False #If False, loads a model
+#Number of layers
+NB_CONV_LAYERS = 2
+#Conv + relu
+CONV_FILTERS = 32
+KERNEL_SZ = 3
+CONV_STRIDES = 1
+#Max pool
+CONV_MAX_POOL_SIZE = (2, 2)
+#Dense
+NB_DENSE_LAYERS = 1
+STARTING_UNITS = 128
+UNITS_FACTOR = 2
+EPOCHS = 1 #20
 
 def setup(classifier) :
-    #Add layers
-    classifier.add(Convolution2D(filters=32, kernel_size=3, strides=1,
-        input_shape=[*INPUT_SIZE, 3], activation="relu"))
+    #First, compulsory, layer
+    classifier.add(Convolution2D(filters=CONV_FILTERS, kernel_size=KERNEL_SZ,
+        strides=CONV_STRIDES, input_shape=[*INPUT_SIZE, 3], activation="relu"))
+    classifier.add(MaxPooling2D(pool_size=CONV_MAX_POOL_SIZE))
+    #Other layers
+    for i in range(1, NB_CONV_LAYERS):
+        classifier.add(Convolution2D(filters=CONV_FILTERS,
+            kernel_size=KERNEL_SZ, strides=CONV_STRIDES, activation="relu"))
+        classifier.add(MaxPooling2D(pool_size=CONV_MAX_POOL_SIZE))
+    classifier.add(Flatten())
 
     """
     kernel_size : 3, 5, 7…
@@ -19,11 +39,11 @@ def setup(classifier) :
     input_shape : on force toutes les images à la même taille
     """
 
-    classifier.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-    classifier.add(Flatten())
-
     #Fully Connected Network
-    classifier.add(Dense(units=128, activation="relu"))
+    for i in range(NB_DENSE_LAYERS):
+        classifier.add(Dense(units=STARTING_UNITS*UNITS_FACTOR**i,
+            activation="relu"))
+    #Last layer :
     classifier.add(Dense(units=1, activation="sigmoid"))
     # classifier.add(Dense(units=2, activation="softmax"))
 
@@ -31,15 +51,13 @@ def setup(classifier) :
     classifier.compile(optimizer="adam", loss="binary_crossentropy",
         metrics=["accuracy"])
 
-
-def train(classifier):
-    # Augmentation d'images
+def get_sets():
     train_datagen = ImageDataGenerator(
             rescale=1./255, #Valeur pixel entre 0 et 1
             shear_range=0.2, #Transvection
             zoom_range=0.2,
             horizontal_flip=True)
-    test_datagen = ImageDataGenerator(rescale=1./255) #Tout sur la même échelle !
+    test_datagen = ImageDataGenerator(rescale=1./255) # Même échelle !
     training_set = train_datagen.flow_from_directory(
             'data/train',
             target_size=INPUT_SIZE,
@@ -50,23 +68,29 @@ def train(classifier):
             target_size=INPUT_SIZE,
             batch_size=32,
             class_mode='binary')
+    return training_set, test_set
 
-    #Trainging itself
+def train(classifier, training_set):
     classifier.fit(
             training_set,
             steps_per_epoch=625, #625*32 = 20k
-            epochs=20,
-            validation_data=test_set,
+            epochs=EPOCHS,
             validation_steps=157) #156*32 ~= 5k
+
+def print_score(classifier, test_set):
+    score = classifier.evaluate(test_set, verbose=0)
+    print(f'Test loss: {score[0]:.3f} / Test accuracy: {score[1]:.3f}')
 
 def main():
     classifier = Sequential()
-    setup(classifier)
+    training_set, test_set = get_sets()
     if NEW_MODEL :
-        train(classifier)
-        classifier.save('tests/model0')
+        setup(classifier)
+        train(classifier, training_set)
+        save_model(classifier, 'tests/model0.h5')
     else:
-        classifier.load('tests/model0')
+        classifier = load_model('tests/model0.h5')
+    print_score(classifier, test_set)
 
 if __name__ == "__main__":
     main()
